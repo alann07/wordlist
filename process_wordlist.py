744c3586ebda8d628ea6776b0aa57f75f3300bd5
alany07 <alany@vulcan.com>
2019-06-14 00:35:18 -0700

    add feature to check entire grid one by one.

--- process_wordlist.py
+++ process_wordlist.py
@@ -15,23 +15,10 @@ def parse_args():
     parser.add_argument(grid_file_name, help="grid matrix file")
     parser.add_argument('-x', help="x position on grid. integer 1 to 8, default=random int", type=int, choices=range(1, 9))
     parser.add_argument('-y', help="y position on grid, integer 1 to 8, default=random int", type=int, choices=range(1, 9))
+    parser.add_argument('-check_all', help="check entire matrix to find out the longest word", type=int, default=0, choices=range(0, 2))
 
     args = parser.parse_args()
     args_dict = vars(args)
-
-    wordlist_f = None
-    try:
-        wordlist_f = open(args_dict[wordlist_file_name], "r")
-    except Exception as e:
-        logger.error('Wordlist file read error: ' + format(e))
-        sys.exit(-1)
-
-    grid_f = None
-    try:
-        grid_f = open(args_dict[grid_file_name], "r")
-    except Exception as e:
-        logger.error('grid file read error: ' + format(e))
-        sys.exit(-1)
 
     x = args_dict['x']
     if x is None:
@@ -40,15 +27,32 @@ def parse_args():
     if y is None:
         y = random.randint(1, 8)
 
-    return wordlist_f, grid_f, x, y
+    check_all = args_dict['check_all']
+
+    return args_dict[wordlist_file_name], args_dict[grid_file_name], x, y, check_all
 
 
 def main():
     logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()],
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    wordlist_file, grid_file, x, y = parse_args()
-    wordlist_service = WordlistService()
-    longest_word = wordlist_service.find_longest_word(wordlist_file, grid_file, x, y)
+    logger.info("Start Processing Fantastic Word List")
+    wordlist_file_name, grid_file_name, x, y, check_all = parse_args()
+    wordlist_service = WordlistService(wordlist_file_name, grid_file_name)
+    max_len = 0
+    longest_word = ''
+    if check_all:
+        logger.info("check entire grid")
+        for x1 in range(1, wordlist_service.MATRIX_MAX_COLS+1):
+            for y1 in range(1, wordlist_service.MATRIX_MAX_ROWS + 1):
+                long_word = wordlist_service.find_longest_word(x1, y1)
+                if len(long_word) > max_len:
+                    max_len = len(long_word)
+                    longest_word = long_word
+                    x, y = x1, y1
+
+    else:
+        longest_word = wordlist_service.find_longest_word(x, y)
+
     if longest_word is None or len(longest_word) == 0:
         logger.info("No word found starting (" + str(x) + ", " + str(y) + ")")
     else:
--- wordlist_service.py
+++ wordlist_service.py
@@ -6,35 +6,35 @@ logger = logging.getLogger(__name__)
 
 class WordlistService:
 
-    def __init__(self):
+    def __init__(self, wordlist_file_name, grid_file_name):
         self.MATRIX_MAX_ROWS = 8
         self.MATRIX_MAX_COLS = 8
         self.KNIGH_MOVES = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
 
-    def find_longest_word(self, wordlist_file, matrix_file, x, y):
+        self.word_trie = self.create_trie_from_file(wordlist_file_name)
+        self.matrix = self.create_matrix_from_file(grid_file_name)
 
-        word_trie = self.create_trie_from_file(wordlist_file)
-        matrix = self.create_matrix_from_file(matrix_file)
+    def find_longest_word(self, x, y):
 
         i_idx = y - 1
         j_idx = x - 1
 
         word_queue = []
-        word_queue.insert(0, (matrix[i_idx][j_idx].lower(), i_idx, j_idx))
+        word_queue.insert(0, (self.matrix[i_idx][j_idx].lower(), i_idx, j_idx))
 
         long_word_list = []
 
         while len(word_queue) > 0:
             curr_item = word_queue.pop()
             curr_str = curr_item[0]
-            is_curr_str_exist, is_curr_str_reaching_word_end = word_trie.search(curr_str)
+            is_curr_str_exist, is_curr_str_reaching_word_end = self.word_trie.search(curr_str)
             if is_curr_str_exist:
                 if is_curr_str_reaching_word_end:
                     long_word_list.append(curr_str)
                 knight_next_move_positions = self.get_next_knight_move_positions(curr_item[1], curr_item[2])
 
                 for next_pos in knight_next_move_positions:
-                    word_queue.insert(0, (curr_str + matrix[next_pos[0]][next_pos[1]].lower(), next_pos[0], next_pos[1]))
+                    word_queue.insert(0, (curr_str + self.matrix[next_pos[0]][next_pos[1]].lower(), next_pos[0], next_pos[1]))
 
         longest_word = ''
         max_len = 0
@@ -44,19 +44,22 @@ class WordlistService:
                 longest_word = word
         return longest_word
 
-    def create_trie_from_file(self, wordlist_file):
-        if wordlist_file is None:
-            return None
+    def create_trie_from_file(self, wordlist_file_name):
+        wordlist_file = None
+        try:
+            wordlist_file = open(wordlist_file_name, "r")
+            wtrie = Trie()
 
-        wtrie = Trie()
-
-        for line in wordlist_file:
-            if line is None or len(line.strip()) == 0:
-                continue
-
-            self.insert_line_to_trie(wtrie, line.strip())
-
-        return wtrie
+            for line in wordlist_file:
+                if line is None or len(line.strip()) == 0:
+                    continue
+                self.insert_line_to_trie(wtrie, line.strip())
+            return wtrie
+        except Exception as e:
+            logger.error('Wordlist file read error: ' + format(e))
+            sys.exit(-1)
+        else:
+            wordlist_file.close()
 
     def insert_line_to_trie(self, wtrie, line):
         i = 0
@@ -76,22 +79,30 @@ class WordlistService:
             curr_word = line[start_ptr: end_ptr + 1]
             wtrie.insert(curr_word.strip().lower())
 
-    def create_matrix_from_file(self, matrix_file):
-        matrix = [[0 for m in range(8)] for n in range(8)]
+    def create_matrix_from_file(self, grid_file_name):
+        grid_file = None
+        try:
+            grid_file = open(grid_file_name, "r")
+            matrix = [[0 for m in range(8)] for n in range(8)]
 
-        i = 0
-        for line in matrix_file:
-            if i >= self.MATRIX_MAX_ROWS:
-                sys.exit("exceeding row boundary " + self.MATRIX_MAX_ROWS)
-            j = 0
-            for char in line.split(' '):
-                if j > self.MATRIX_MAX_COLS:
-                    sys.exit("exceeding column boundary " + self.MATRIX_MAX_COLS)
-                matrix[i][j] = char.strip()
-                j += 1
-            i += 1
+            i = 0
+            for line in grid_file:
+                if i >= self.MATRIX_MAX_ROWS:
+                    sys.exit("exceeding row boundary " + self.MATRIX_MAX_ROWS)
+                j = 0
+                for char in line.split(' '):
+                    if j > self.MATRIX_MAX_COLS:
+                        sys.exit("exceeding column boundary " + self.MATRIX_MAX_COLS)
+                    matrix[i][j] = char.strip()
+                    j += 1
+                i += 1
 
-        return matrix
+            return matrix
+        except Exception as e:
+            logger.error('grid file read error: ' + format(e))
+            sys.exit(-1)
+        else:
+            grid_file.close()
 
     def get_next_knight_move_positions(self, i, j):
         next_positions = []
